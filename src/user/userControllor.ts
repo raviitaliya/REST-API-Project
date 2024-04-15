3ffd3865df1a2a8c406488ef65eb44ac4fdf0167
raviitaliya <italiyaravi704@gmail.com>
2024-04-15 20:27:46 +0530

    email veryfiy added

--- src/user/userControllor.ts
+++ src/user/userControllor.ts
@@ -10,6 +10,14 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "all filds are required..");
     return next(error);
   }
+  
+  //email verfiy
+  const isEmailExist = await userModel.findOne({ email });
+
+  if (isEmailExist) {
+    const error = createHttpError(400, "user is alredy exist...");
+    return next(error);
+  }
 
   
 
